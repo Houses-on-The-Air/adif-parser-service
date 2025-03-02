0914195dd5bd90419351f272e3b06833e3710865
Antony Bailey <antonybailey@Antonys-MacBook-Pro.local>
2025-03-02 14:38:37 +0000

    minor

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI, File, UploadFile
+
 from adif_parser import parse_adif
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the ADIF service. Visit /docs for the API documentation."}
+
+
 @app.post("/upload_adif/")
-async def upload_adif(file: UploadFile = File(...)):
+async def upload_adif(file: UploadFile = None):
     """
     Asynchronously uploads and processes an ADIF (Amateur Data Interchange Format) file.
 
@@ -17,6 +23,8 @@ async def upload_adif(file: UploadFile = File(...)):
     Raises:
         UnicodeDecodeError: If the file content cannot be decoded using UTF-8.
     """
+    if file is None:
+        file = File(...)
     content = await file.read()
     file_content = content.decode("utf-8")
     result = parse_adif(file_content)
